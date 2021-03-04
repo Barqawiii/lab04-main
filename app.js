9fb4bd6ef47759b068d51fc36e01cd25db698491
barq <mohammad_barqawi@hotmail.com>
2021-03-04 14:29:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+ 
+ 
+ var birth_year =prompt("please enter your birth year :D ");
+ var This_year=2021;
+ if (birth_year<=This_year && birth_year > 0)
+ {
+    var Age =This_year - birth_year;
+    alert("you age is "+ Age);
+ }
+  else {
+    alert("not correct Number");
+  }
+  alert("DONE BY BARQAWI :D ");
